8c6a0c61fcae6fe70ddab29d7cd1e59476befbec
emeka-chukwu <emekapascal11@gmail.com>
2024-02-11 15:08:14 +0100

    removed unused method

--- auths-services/util/validation.go
+++ auths-services/util/validation.go
@@ -26,39 +26,6 @@ func ValidateInput(payload interface{}) (map[string]string, error) {
 	return errors, err
 }
 
-func ValidatorMiddleware[T any](w http.ResponseWriter, r *http.Request) {
-	var payload T
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		barf.Response(w).Status(http.StatusBadRequest).JSON(barf.Res{
-			Status:  false,
-			Data:    nil,
-			Message: "error",
-		})
-		return
-	}
-	if err := json.Unmarshal(body, &payload); err != nil {
-		barf.Response(w).Status(http.StatusBadRequest).JSON(barf.Res{
-			Status:  false,
-			Data:    nil,
-			Message: "error",
-		})
-		return
-	}
-
-	_, err = ValidateInput(payload)
-	if err != nil {
-		barf.Response(w).Status(http.StatusBadRequest).JSON(barf.Res{
-			Status:  false,
-			Data:    nil,
-			Message: "error",
-		})
-		return
-	}
-	// c.Set("body", payload)
-
-}
-
 func InputValidatorMiddleware[T any](next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var payload T
